912b3af38e4a918cf25f0f818e558eb9f00883d8
allexis <allexissoares@gmail.com>
2021-09-04 11:47:48 -0300

    :sparkles: feat: update CombineVC to use cards, animation and use array of cards connected

--- tinder/Controller/CombineVC.swift
+++ tinder/Controller/CombineVC.swift
@@ -8,36 +8,67 @@
 import UIKit
 
 class CombineVC: UIViewController {
+    var users: [User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.systemGroupedBackground
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
-        
-        redView.center = view.center
-        
-        let gesture = UIPanGestureRecognizer()
-        gesture.addTarget(self, action: #selector(handleCard))
-        
-        redView.addGestureRecognizer(gesture)
-        
-        view.addSubview(redView)
+        self.searchUsers()
+    }
+    
+    func searchUsers() {
+        self.users = UserService.shared.searchUsers()
+        self.addCards()
+    }
+}
+
+extension CombineVC {
+    func addCards() {
+        for user in users {
+            let card = CombineCardView()
+            card.frame = CGRect(x: 0, y: 0, width: view.bounds.width - 32, height: view.bounds.height * 0.7)
+            
+            card.center = view.center
+            card.user = user
+            card.tag = user.id
+            
+            let gesture = UIPanGestureRecognizer()
+            gesture.addTarget(self, action: #selector(handleCard))
+            
+            card.addGestureRecognizer(gesture)
+            
+            view.insertSubview(card, at: 0)        }
     }
 }
 
 extension CombineVC {
     @objc func handleCard (_ gesture: UIPanGestureRecognizer) {
-        if let card = gesture.view {
+        if let card = gesture.view as? CombineCardView {
             let point = gesture.translation(in: view)
             
             card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
             
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            if point.x > 0 {
+                card.likeImageView.alpha = rotationAngle * 5
+                card.deslikeImageView.alpha = 0
+            } else {
+                card.deslikeImageView.alpha = rotationAngle * 5 * -1
+                card.likeImageView.alpha = 0
+            }
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
             if gesture.state == .ended {
                 UIView.animate(withDuration: 0.2) {
                     card.center = self.view.center
+                    card.transform = .identity
+                    
+                    card.likeImageView.alpha = 0
+                    card.deslikeImageView.alpha = 0
                 }
             }
         }
