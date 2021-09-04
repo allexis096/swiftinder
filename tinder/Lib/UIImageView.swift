5748da9a6725de7f62a1a9cfb12cdc7eba2e198f
allexis <allexissoares@gmail.com>
2021-09-04 11:46:01 -0300

    :sparkles: feat: create UIImage extension

---
+++ tinder/Lib/UIImageView.swift
@@ -0,0 +1,32 @@
+//
+//  UIImageView.swift
+//  tinder
+//
+//  Created by allexis on 04/09/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    static func photoImageView(named: String? = nil) -> UIImageView {
+        let imageView = UIImageView()
+        
+        if let named = named {
+            imageView.image = UIImage(named: named)
+        }
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }
+    
+    static func iconCard(named: String) -> UIImageView {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: named)
+        imageView.size(size: .init(width: 70, height: 70))
+        imageView.alpha = 0.0
+        
+        return imageView
+    }
+}
