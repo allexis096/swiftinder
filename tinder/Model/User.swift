3b4062cdf3945e7adb87212180280030392c49da
allexis <allexissoares@gmail.com>
2021-09-04 11:44:53 -0300

    :sparkles: feat: create User model

---
+++ tinder/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  tinder
+//
+//  Created by allexis on 04/09/21.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let name: String
+    let age: Int
+    let match: Bool
+    let phrase: String
+    let photo: String
+}
